22a6cc283d2300d008561927af6bcf4f85f22a46
Anthony Rurup <tcrurup@gmail.com>
2019-04-25 22:41:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,6 @@ def assign_rooms(speakers_array)
   speakers_array.each_with_index{ 
     |name, index| assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   }
-  assigned_rooms
 end
 test_names = ["Tony", "Cody", "Nathan", "Nazif"]
 final_test = assign_rooms(test_names)
