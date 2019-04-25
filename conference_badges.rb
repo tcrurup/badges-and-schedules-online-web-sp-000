ca6098f9b3e6a33706b943a804c97778df3a134f
Anthony Rurup <tcrurup@gmail.com>
2019-04-25 21:22:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,14 @@
 
+require 'pry'
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(guest_name_array)
+  guest_badges = guest_name_array.collect{ |name| badge_maker(name)
+  binding.pry
+end
+
+test_names = ["Tony", "Cody", "Nathan", "Nazif"]
+batch_badge_creator(test_names)
